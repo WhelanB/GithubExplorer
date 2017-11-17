e39e6875d579105a29145a537a9b0056742bdad8
Brian Whelan <whelanb7@tcd.ie>
2017-11-17 12:50:40 +0000

    Initial Commit, Created Base ASP.NET Core project

---
+++ GitHub/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Octokit;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
